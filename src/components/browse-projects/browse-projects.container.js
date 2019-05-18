33b3fd57978b95626d8281b25e0802cca43bc241
Sagar Thakore <sagart12@gmail.com>
2019-05-18 15:29:27 -0400

    Update browse-projects.container.js
    fix sorting by data quality score on the browse projects page

--- src/components/browse-projects/browse-projects.container.js
+++ src/components/browse-projects/browse-projects.container.js
@@ -1,25 +1,21 @@
 import { connect } from 'react-redux'
-import { includes } from '@code.gov/cautious'
+import { includes , some } from '@code.gov/cautious'
 import get from 'lodash.get'
-import {
-  getFilterTags,
-  getFilterValuesFromParamsByCategory,
-  normalize
-} from 'utils/other'
+import { getFilterTags, getFilterValuesFromParamsByCategory, normalize } from 'utils/other'
 import saveFilterOptions from 'actions/save-filter-options'
 import updateBrowseFilters from 'actions/update-browse-filters'
 import updateBrowseParams from 'actions/update-browse-params'
+import { sortByBestMatch, sortByDataQuality, sortByDate, sortByName } from 'utils/repo-sorting'
 import BrowseProjectsComponent from './browse-projects.component'
 
-export const mapStateToProps = ({ browseParams, browseResults, filters }) => {
 
+export const mapStateToProps = ({ browseParams, browseResults, filters }) => {
   const categories = ['agencies', 'languages', 'licenses', 'usageTypes']
 
   const selections = categories.reduce((accumulator, key) => {
     accumulator[key] = getFilterValuesFromParamsByCategory(browseParams, key)
     return accumulator
   }, {})
-  //console.log("selections:", selections)
 
   const selectedSorting = browseParams.sort
   const selectedPage = browseParams.page
@@ -28,9 +24,7 @@ export const mapStateToProps = ({ browseParams, browseResults, filters }) => {
   let boxes = {}
   if (filters) {
     boxes = categories.reduce((accumulator, key) => {
-      accumulator[key] = filters[key].map(({ name, value}) => {
-        return { name, value, checked: includes(selections[key], normalize(value)) }
-      })
+      accumulator[key] = filters[key].map(({ name, value }) => ({ name, value, checked: includes(selections[key], normalize(value)) }))
       return accumulator
     }, {})
   }
@@ -38,11 +32,78 @@ export const mapStateToProps = ({ browseParams, browseResults, filters }) => {
   const total = get(browseResults, 'total') || 0
   const repos = get(browseResults, 'repos')
 
+  let searchResults
+  if (repos) {
+    searchResults = repos
+      .sort((a, b) => {
+        if (selectedSorting === 'best_match') {
+          return sortByBestMatch(a, b)
+        } if (selectedSorting === 'data_quality') {
+          return sortByDataQuality(a, b)
+        } if (selectedSorting === 'a-z') {
+          return sortByName(a, b)
+        } if (selectedSorting === 'last_updated') {
+          return sortByDate(a, b)
+        }
+      })
+      .filter(repo => {
+        if (filters) {
+          if (
+            some(selections.agencies) &&
+            !selections.agencies.includes(normalize(repo.agency.acronym))
+          ) {
+            return false
+          }
+
+          if (
+            some(selections.languages) &&
+            !overlaps(normalize(repo.languages), selections.languages)
+          ) {
+            return false
+          }
+
+          if (some(selections.licenses)) {
+            // no licenses assigned on the repo
+            if (hasLicense(repo) === false) {
+              return false
+            }
+
+            const repoLicenses = repo.permissions.licenses.map(license => normalize(license.name))
+            if (!overlaps(repoLicenses, selections.licenses)) {
+              return false
+            }
+          }
+
+          const normalizedRepoUsageType = normalize(repo.permissions.usageType)
+          if (
+            some(selections.usageTypes) &&
+            !selections.usageTypes.includes(normalizedRepoUsageType)
+          ) {
+            return false
+          }
+
+          // don't want to visualize exempt repos
+          if (normalizedRepoUsageType.includes('exempt')) {
+            return false
+          }
+
+          return true
+        }
+
+        return false
+      })
+
+    searchResults = searchResults.slice(
+      (selectedPage - 1) * selectedPageSize,
+      selectedPage * selectedPageSize
+    )
+  }
+
   const sortOptions = [
     {
       label: 'Data Quality',
       value: 'data_quality',
-      selected: selectedSorting=== 'data_quality'
+      selected: selectedSorting === 'data_quality'
     },
     {
       label: 'A-Z',
@@ -61,7 +122,7 @@ export const mapStateToProps = ({ browseParams, browseResults, filters }) => {
   const result = {
     boxes,
     browseParams,
-    browseResults,
+    searchResults,
     filterTags,
     selectedSorting,
     repos,
@@ -71,12 +132,10 @@ export const mapStateToProps = ({ browseParams, browseResults, filters }) => {
     total
   }
 
-  //console.log("browse-projects's container passing following to component:", result)
   return result
 }
 
-export const mapDispatchToProps = dispatch => {
-  return {
+export const mapDispatchToProps = dispatch => ({
     onFilterBoxChange: (category, change) => {
       dispatch(updateBrowseFilters(category, change.value, change.type))
     },
@@ -90,7 +149,9 @@ export const mapDispatchToProps = dispatch => {
     updatePage: newPage => {
       dispatch(updateBrowseParams({ page: newPage }))
     }
-  }
-}
+  })
 
-export default connect(mapStateToProps, mapDispatchToProps)(BrowseProjectsComponent)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(BrowseProjectsComponent)
